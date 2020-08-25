eb5bf6ad57f3d2f02a371c61b37a60d592cf3d1c
Maxime Ealet <maxime.ealet@ezeeworld.com>
2020-08-25 14:04:11 +0200

    Update to code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,52 @@
-import React from "react"
+import React from 'react';
+import Helmet from 'react-helmet';
+import {useStaticQuery, graphql} from 'gatsby';
+import Img from 'gatsby-image';
+import '../scss/_landing.scss';
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+const Index = () => {
+  const data = useStaticQuery(graphql`
+      {
+        datoCmsSite {
+          globalSeo {
+            siteName
+            facebookPageUrl
+            fallbackSeo {
+              description
+            }
+            titleSuffix
+          }
+          locale
+        }
+      }
+  `);
+
+  const { datoCmsSite } = data;
+  let siteName = datoCmsSite.globalSeo.siteName;
+  let siteTitle = siteName + datoCmsSite.globalSeo.titleSuffix;
+  let siteDescription = datoCmsSite.globalSeo.fallbackSeo.description;
+
+
+  return (
+      <>
+      <Helmet
+            htmlAttributes={{
+                lang   : 'fr',
+                prefix : 'og: http://ogp.me/ns#'
+            }}
+        >
+            {/* General tags */}
+            <title>{siteTitle}</title>
+            <meta name="description" content={siteDescription} />
+             {/* OpenGraph tags */}
+             <meta property="og:site_name" content={siteTitle} />
+            <body id="landing" />
+        </Helmet>
+          <div id="content-wrapper">
+            YOP
+          </div>
+      </>
+  );
+};
+
+export default Index;
