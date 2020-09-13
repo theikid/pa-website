5995ce45d681a7494e0cd4081816bd97bf0445a9
Maxime Ealet <theikid@theikid.local>
2020-09-13 23:56:01 +0200

    Modify smooth scroll menu closing v4

--- src/html.js
+++ src/html.js
@@ -27,7 +27,7 @@ export default function HTML(props) {
     __html: `
     var scroll = new SmoothScroll('a[href*="#"]', {
    
-      offset: 282,
+      offset: 300,
       speed: 300,
       
       // History
